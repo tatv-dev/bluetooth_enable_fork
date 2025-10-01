230bc1a82e6ddcdcbe9c2ca9f5d50436b82d79e1
DESKTOP-SRO79J6\ADMIN <ngocanhdt.icv@gmail.com>
2025-10-01 13:56:18 +0700

    fix build

---
+++ android/src/main/java/com/hui/bluetooth_enable/BluetoothEnablePlugin.kt
@@ -0,0 +1,25 @@
+package com.hui.bluetooth_enable
+
+import android.Manifest
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry.ActivityResultListener
+import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener
+
+/** BluetoothEnablePlugin  */
+internal class BluetoothEnablePlugin
